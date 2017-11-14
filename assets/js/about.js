99bbfcde4fbb778ef402065486f48b487be55d4b
Kevin D. Silva <kevin.dasilva1@gmail.com>
2017-11-14 11:53:25 -0500

    saving cause

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,4 +1,13 @@
-const mydata = JSON.parse(data);
-
-
-console.log(mydata[0]);
+// circle graphs ///
+$(function() {
+    $('.circle-graph').easyPieChart({
+      scaleColor: false,
+      lineWidth: 20,
+      lineCap: 'butt',
+      barColor: '#a378aa',
+      trackColor: '#e7b8ef' ,
+      size: 150,
+      animate: 800
+    });
+  });
+///////////////
