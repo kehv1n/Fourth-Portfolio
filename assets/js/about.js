66805e7060bd5108c17a91d8661dab10f45eb870
Kevin da Silva <kevindasilva@MacBook-Pro.local>
2019-11-14 19:06:54 +0100

    - Updated Avi Pic - Removed depricated projects (ie ParkrR) - Styles Update - Removed const and deprecated functions -

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,4 +1,48 @@
-const mydata = JSON.parse(data);
+const data =  [
+    {
+        "exerpt": "A man cannot build a reputation on what he is going to do.",
+        "author": "Henry Ford"
+    },
+    {
+        "exerpt": "My mother said to me, 'If you are a soldier, you will become a general. If you are a monk, you will become the Pope.' Instead, I was a painter, and became Picasso.",
+        "author": "Pablo Picasso"
+    },
+    {
+        "exerpt": "We are what we repeatedly do. Excellence then, is not an act, but a habit.",
+        "author": "Aristotle"
+    },
+    {
+        "exerpt": "A great man shows his greatness by the way he treats little men.",
+        "author": "Thomas Carlyle"
+    },
+    {
+        "exerpt": "A computer would deserve to be called intelligent if it could deceive a human into believing that it was human.",
+        "author": "Alan Turing"
+    },
+    {
+        "exerpt": "Believe you can and you're halfway there.",
+        "author": "Theodore Roosevelt"
+    },
+    {
+        "exerpt": "I am still learning.",
+        "author": "Michelangelo di Lodovico Buonarroti Simoni"
+    },
 
 
-console.log(mydata[0]);
+
+]
+
+// const mydata = JSON.parse(data);
+
+
+// every 10 seconds, swap quote with quote randomly selected from above
+// var quoteSwap = () => {
+//   // select random quote
+//   let arr_width = data.length();
+//   console.log(arr_width);
+//
+//   // put onto screen
+//   // take width of div
+//   // adjust width of div = width of span
+//   // fill in quote author
+// }
